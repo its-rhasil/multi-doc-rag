3159e31e58ac3413ac8fdd73178db490c7c64a65
Muhammed Rhasil <muhammed.rhasil7@gmail.com>
2026-07-22 12:50:22 +0530

    feat: add splitting documents texts into smaller chunks

---
+++ app/rag/chunk.py
@@ -0,0 +1,19 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+
+def chunk(pages: list, chunk_size: int = 1000, chunk_overlap: int = 200):
+    textsplitter = RecursiveCharacterTextSplitter(
+        chunk_size = chunk_size,
+        chunk_overlap = chunk_overlap,
+        length_function = len
+    )
+    documents = []
+    for page in pages:
+        document = Document(
+            page_content=page["text"],
+            metadata = page["metadata"]
+        )
+        documents.append(chunk)
+    chunks = textsplitter(documents)
+
+    return chunks
